cbb002a2b88f85b85ef65551564d1eae4042a12a
Ashley <ashleywong678@gmail.com>
2019-11-27 22:44:30 -0500

    completed challenge to parse and concatinate parts of the json

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/37.8267,-122.4233'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    const temp = response.body.currently.temperature
+    const precip = response.body.currently.precipProbability
+    console.log(`It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
 })
+
+// Challenge: Print a small forecast to the user
+// 1. Print "It is currently 58 degress out. There is a 0% chance of rain."
+// 2. Test your work!
