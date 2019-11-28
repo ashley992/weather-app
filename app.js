1a446cb680ef6363a1db413e01a14aaf31e94d9b
Ashley <ashleywong678@gmail.com>
2019-11-27 22:50:57 -0500

    concat data summary to previous console.log

--- app.js
+++ app.js
@@ -5,9 +5,5 @@ const url = 'https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/3
 request({ url: url, json: true }, (error, response) => {
     const temp = response.body.currently.temperature
     const precip = response.body.currently.precipProbability
-    console.log(`It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
+    console.log(response.body.daily.data[0].summary + ` It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
 })
-
-// Challenge: Print a small forecast to the user
-// 1. Print "It is currently 58 degress out. There is a 0% chance of rain."
-// 2. Test your work!
