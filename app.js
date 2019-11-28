255befa6290067d5098c14a1d8de97351a6cd21d
Ashley <ashleywong678@gmail.com>
2019-11-28 07:57:16 -0500

    error handling challenge

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/37.8267,-122.4233'
 
-request({ url: url, json: true }, (error, response) => {
-    const temp = response.body.currently.temperature
-    const precip = response.body.currently.precipProbability
-    // console.log(response.body.daily.data[0].summary + ` It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
-})
+// request({ url: url, json: true }, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to weather services.')
+//     } else if(response.body.error){
+//         console.log('Unable to find location.')
+//     } else {
+//         const temp = response.body.currently.temperature
+//         const precip = response.body.currently.precipProbability
+//         console.log(response.body.daily.data[0].summary + ` It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
+//     }
 
-// Challenge: Geocoding
-// Address -> Lat/Long -> Weather
+// })
 
-// Goal: Print the lat/long for LA
-// 1. Fire off a new request to theURL explored in browser
-// 2. Have the Request module prase it as JSON
-// 3. Print both the Lat and Long to the terminal
-// 4. Test your work
+// Challenge: Handle errors for geocoding request
+// 1. Setup an error handle for low-level errors
+// 2. Test by disabling network request and running the app
+// 3. Setup error handling for no matching results
+// 4. Test by altering the search term and running the app
 
 const mapURL = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXNobGV5d29uZzY3OCIsImEiOiJjazNpNm8zd3gwNTh2M2hwaHB4bnE0Y3M4In0.X_KoS5NrgwUMUm0nipDK2A&limit=1'
 
 request( { url: mapURL, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[0]
-    const longitude = response.body.features[0].center[1]
-    console.log(`Latitude is ${latitude}. Longitude is ${longitude}.`)
+    if(error){
+        console.log('Unable to connect to location services.')
+    } else if (response.body.features.length === 0){
+        console.log('Location not found. Try to search again.')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(`Latitude is ${latitude}. Longitude is ${longitude}.`)
+    }
 })
