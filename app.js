8993a2f537ea84f0fe72b8b90944316ef87dd61e
Ashley <ashleywong678@gmail.com>
2019-11-28 21:21:01 -0500

    seperated geocode method into diff file and exported it from utils, imported it into app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/37.8267,-122.4233'
 
@@ -15,22 +16,24 @@ const request = require('request')
 
 // })
 
-// Challenge: Handle errors for geocoding request
-// 1. Setup an error handle for low-level errors
-// 2. Test by disabling network request and running the app
-// 3. Setup error handling for no matching results
-// 4. Test by altering the search term and running the app
 
-const mapURL = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXNobGV5d29uZzY3OCIsImEiOiJjazNpNm8zd3gwNTh2M2hwaHB4bnE0Y3M4In0.X_KoS5NrgwUMUm0nipDK2A&limit=1'
+// const mapURL = 'http://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXNobGV5d29uZzY3OCIsImEiOiJjazNpNm8zd3gwNTh2M2hwaHB4bnE0Y3M4In0.X_KoS5NrgwUMUm0nipDK2A&limit=1'
 
-request( { url: mapURL, json: true}, (error, response) => {
-    if(error){
-        console.log('Unable to connect to location services.')
-    } else if (response.body.features.length === 0){
-        console.log('Location not found. Try to search again.')
-    } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(`Latitude is ${latitude}. Longitude is ${longitude}.`)
-    }
+// request( { url: mapURL, json: true}, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to location services.')
+//     } else if (response.body.features.length === 0){
+//         console.log('Location not found. Try to search again.')
+//     } else {
+//         const latitude = response.body.features[0].center[1]
+//         const longitude = response.body.features[0].center[0]
+//         console.log(`Latitude is ${latitude}. Longitude is ${longitude}.`)
+//     }
+// })
+
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXNobGV5d29uZzY3OCIsImEiOiJjazNpNm8zd3gwNTh2M2hwaHB4bnE0Y3M4In0.X_KoS5NrgwUMUm0nipDK2A&limit=1'
+    request( {url: url, json: true}, (error, response) => {
+        if(error){
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.features.length === 0) {
+            callback(', unable to find location. Try another search', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
