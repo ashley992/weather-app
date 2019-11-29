3293d1d5d219297cc40a1f7060a5263c424de7f1
Ashley <ashleywong678@gmail.com>
2019-11-29 09:23:32 -0500

    refactored with destructured props and shorthand attributes

--- app.js
+++ app.js
@@ -2,27 +2,26 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-// Challenge: Accept location via command line argument
-// 1. Access the command line argument without yargs
-// 2. Use the string value as the input for geocode
-// 3. Only geocode if a location was provided
-// 4. Test your work with a few locations
+
+// Challenge: use both destructuring and property shorthand in weather app
+// 1. Use destructuring in app.js, forecast.js, and geocode.js
+// 2. Use propery shorthand in forecast.js and geocode.js
+// 3. Test work and ensure app still works
+
 const location = process.argv[2]
 
 if (!location){
     console.log('Please provide a location.')
 } else {
-    geocode(location, (error, data) => {
+    geocode(location, (error, {latitude, longitude, location}) => {
         if(error){
             return console.log(error)
         } 
-        
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return console.log(error)
             }
-    
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
           })
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
     const url = `https://api.darksky.net/forecast/1ddfdc3470e5253d07e9c3e0c923148d/${lat},${long}`
-    request( {url: url, json: true}, (error, response) => {
+    request( {url, json: true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to weather services.', undefined)
-        } else if(response.body.error){
+        } else if(body.error){
             console.log('Unable to find location.')
         } else{
-            const temp = response.body.currently.temperature
-            const precip = response.body.currently.precipProbability
-            callback(undefined, `It is currently ${temp} degrees out. There is a ${precip}% chance of rain.`)
+            // const temp = response.body.currently.temperature
+            // const precip = response.body.currently.precipProbability
+            callback(undefined, `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 }
